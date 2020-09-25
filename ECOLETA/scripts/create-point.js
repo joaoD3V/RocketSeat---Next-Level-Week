c830ee1638286ad9d9da9ae786d1539ee68fd098
Joao Pedro <joao.devweb@gmail.com>
2020-09-25 15:57:53 -0300

    Finish Form Create Point

--- ECOLETA/scripts/create-point.js
+++ ECOLETA/scripts/create-point.js
@@ -18,8 +18,6 @@ function getCities(event){
     const citySelect = document.querySelector("select[name=city]");
     const stateInput = document.querySelector("input[name=state]");
 
-    citySelect.innerHTML = ""; // Resetar os dados das cidades a cada seleção
-
     const ufValue = event.target.value;
 
     const indexOfSelectedState = event.target.selectedIndex;
@@ -27,12 +25,15 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"; 
+    citySelect.disabled = true;
+
     fetch(url)
     .then( res => res.json())
     .then( cities => {
 
         for (const city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
         }
 
         citySelect.disabled = false;
@@ -46,5 +47,55 @@ document
     .addEventListener("change", getCities);
 
 
+// Itens de coleta
+// Pegar todos os li's
 
-// 2:05:40
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem);
+}
+
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event){
+    const itemLi = event.target;
+
+    // Adicionar ou remover uma classe com JavaScript
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+   
+
+    // Verificar se existem items selecionados, se sim, pegar itens selecionados
+    const alredySelected = selectedItems.findIndex(item => {
+        // const alredySelected = selectedItems.findIndex(item => item == itemId); // Jeito mais simples de fazer a Arrow Function que retorna apenas uma coisa
+        const itemFound = item == itemId; //Isso será true ou false
+        return itemFound;
+    });    
+
+
+    // Se Já estiver selecionado, 
+    if(alredySelected >= 0){
+        //tirar da selecao
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId; 
+            return itemIsDifferent;
+        });
+
+        selectedItems = filteredItems;
+    } else{
+         // Se não estiver selecionado, adicionar à seleção
+         selectedItems.push(itemId);
+        
+    }
+    
+    // Atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems;
+    
+
+}
