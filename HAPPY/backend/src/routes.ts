124d7102b838302bd240dd0b18903f6943a3bf04
Joao Pedro <joao.devweb@gmail.com>
2020-11-14 14:56:44 -0300

    Add inital config of MVC

---
+++ HAPPY/backend/src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+
+routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+
+
+
+
+export default routes;
