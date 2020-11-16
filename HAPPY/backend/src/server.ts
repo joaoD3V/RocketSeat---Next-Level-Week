9dddb9d6960e03b81a4cb5b832d1e0b44150e019
Joao Pedro <joao.devweb@gmail.com>
2020-11-16 13:10:19 -0300

    Add handler errors

--- HAPPY/backend/src/server.ts
+++ HAPPY/backend/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
+
+
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 
 const app = express();
@@ -10,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 
 
