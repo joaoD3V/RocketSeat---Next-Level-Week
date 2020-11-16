481d33a894a1810d48f3196cad4e0f65f0a87bc0
Joao Pedro <joao.devweb@gmail.com>
2020-11-16 14:36:08 -0300

    Finish backend

--- HAPPY/backend/src/server.ts
+++ HAPPY/backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 
@@ -11,6 +13,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
